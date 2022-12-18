3b7d4ba6ac07760910c7326732cef3d0eda21c96
Emilio Bertò <emilio.berto1@gmail.com>
2022-12-18 21:26:49 +0100

    8 part 2 TODO: FIX THIS 💩💩💩💩💩

--- src/8/index.ts
+++ src/8/index.ts
@@ -26,13 +26,13 @@ for (let i = 1; i < map.length - 1; i++) {
         const leftCheck = map[i]
             .filter((value, index) => index < j)
             .every((value, index) => {
-           return value < elementToCheck ;
-        });
+                return value < elementToCheck;
+            });
         const rightCheck = map[i]
             .filter((value, index) => index > j)
             .every((value, index) => {
-            return value < elementToCheck;
-        });
+                return value < elementToCheck;
+            });
 
         const verticalRow: number[] = []
         for (let k = 0; k < map.length; k++) {
@@ -44,15 +44,15 @@ for (let i = 1; i < map.length - 1; i++) {
                 return index < i
             })
             .every((value, index, array) => {
-            return value < elementToCheck;
-        });
+                return value < elementToCheck;
+            });
         const lowerCheck = verticalRow
             .filter((value, index) => {
                 return index > i
             })
             .every((value, index, array) => {
-            return value < elementToCheck;
-        });
+                return value < elementToCheck;
+            });
 
         if (leftCheck || rightCheck || lowerCheck || upperCheck) {
             visibleTrees++;
@@ -62,7 +62,92 @@ for (let i = 1; i < map.length - 1; i++) {
 
 console.log(visibleTrees);
 
-debugger;
 // Part 2
+
+let maxScore = 0;
+for (let i = 1; i < map.length - 1; i++) {
+    for (let j = 1; j < map[0].length - 1; j++) {
+        const elementToCheck = map[i][j];
+
+        let shouldStop = false;
+        let leftScore = 0;
+        while (!shouldStop) {
+            leftScore--;
+            if (j + leftScore < 0) {
+                shouldStop = true;
+                leftScore++;
+                continue;
+            }
+
+            if (map[i][j - leftScore] >= elementToCheck) {
+                shouldStop = true;
+            }
+        }
+        if (leftScore < 0) {
+            leftScore *= -1;
+        }
+
+        shouldStop = false;
+        let rightScore = 0;
+        while (!shouldStop) {
+            rightScore++;
+            if (j + rightScore >= map[0].length) {
+                shouldStop = true;
+                rightScore--;
+                continue;
+            }
+
+            if (map[i][j + rightScore] >= elementToCheck) {
+                shouldStop = true;
+            }
+        }
+
+        const verticalRow: number[] = []
+        for (let k = 0; k < map.length; k++) {
+            verticalRow.push(map[k][j]);
+        }
+
+        shouldStop = false;
+        let upperScore = 0;
+        while (!shouldStop) {
+            upperScore--;
+            if (i + upperScore < 0) {
+                shouldStop = true;
+                upperScore++;
+                continue;
+            }
+
+            if (verticalRow[i - upperScore] < elementToCheck) {
+                shouldStop = true;
+            }
+        }
+        if (upperScore < 0) {
+            upperScore *= -1;
+        }
+
+        shouldStop = false;
+        let lowerScore = 0;
+        while (!shouldStop) {
+            lowerScore++;
+            if (i + lowerScore >= verticalRow.length) {
+                shouldStop = true;
+                lowerScore--;
+                continue;
+            }
+
+            if (verticalRow[i + lowerScore] >= elementToCheck) {
+                shouldStop = true;
+            }
+        }
+
+        const totalScore = leftScore * rightScore * upperScore * lowerScore;
+        if (totalScore > maxScore) {
+            maxScore = totalScore;
+        }
+    }
+}
+
+console.log(maxScore);
+
 
 // Utils
