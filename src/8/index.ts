3dac4cab1d6096d06ad8caea9b824cdf62fc6f83
Emilio Bertò <emilio.berto1@gmail.com>
2022-12-18 20:51:09 +0100

    8 part 1 ok

--- src/8/index.ts
+++ src/8/index.ts
@@ -10,8 +10,59 @@ for await (const line of readInterface) {
     input.push(line);
 }
 
+const map: number[][] = [];
+input.forEach(row => {
+    map.push(row.split('').map(e => +e));
+})
+
 // Part 1
 
+let visibleTrees = (map.length * 2) + (map[0].length * 2) - 4;
+
+for (let i = 1; i < map.length - 1; i++) {
+    for (let j = 1; j < map[0].length - 1; j++) {
+        const elementToCheck = map[i][j];
+
+        const leftCheck = map[i]
+            .filter((value, index) => index < j)
+            .every((value, index) => {
+           return value < elementToCheck ;
+        });
+        const rightCheck = map[i]
+            .filter((value, index) => index > j)
+            .every((value, index) => {
+            return value < elementToCheck;
+        });
+
+        const verticalRow: number[] = []
+        for (let k = 0; k < map.length; k++) {
+            verticalRow.push(map[k][j]);
+        }
+
+        const upperCheck = verticalRow
+            .filter((value, index) => {
+                return index < i
+            })
+            .every((value, index, array) => {
+            return value < elementToCheck;
+        });
+        const lowerCheck = verticalRow
+            .filter((value, index) => {
+                return index > i
+            })
+            .every((value, index, array) => {
+            return value < elementToCheck;
+        });
+
+        if (leftCheck || rightCheck || lowerCheck || upperCheck) {
+            visibleTrees++;
+        }
+    }
+}
+
+console.log(visibleTrees);
+
+debugger;
 // Part 2
 
 // Utils
