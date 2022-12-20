f84ce351cb6a9e2ce26a1d69cb882a1a842e4c33
Emilio Bertò <emilio.berto1@gmail.com>
2022-12-21 00:33:43 +0100

    Clean as luck

--- src/9/index.ts
+++ src/9/index.ts
@@ -2,109 +2,84 @@ import fs from "fs";
 import readline from "readline";
 
 let input: string[] = [];
-const readInterface = readline.createInterface({
-    input: fs.createReadStream('./resources/9/input')
-});
 
-for await (const line of readInterface) {
-    input.push(line);
-}
+const instructions: [Direction, number][] = await getInstructions('./resources/9/input')
 
-const instructions: [string, number][] = [];
-input.forEach(row => {
-    const [direction, steps] = row.split(' ');
-    instructions.push([direction, +steps]);
-})
+type Direction = 'U' | 'L' | 'D' | 'R';
 
 // Part 1
 
-const size = 1_000_000;
-// let matrix = [];
-// const row = Array(size).fill(null);
-// for (let i = 0; i < size; i++) {
-//     matrix.push([...row]);
-// }
-// matrix[size - 1][0] = 'H';
-
-let headCurrentY = size - 1;
-let headCurrentX = 0;
-
-let tailCurrentY = size - 1;
-let tailCurrentX = 0;
-
-let lastDirection: string | null;
+let headY = 0;
+let headX = 0;
+let tailY = 0;
+let tailX = 0;
+let lastDirection: Direction | null = null;
+let lastHeadX = 0;
+let lastHeadY = 0;
 let visitedByTail: string[] = [];
+const showMatrix = false;
 
 for (const instruction of instructions) {
     const [direction, steps] = instruction;
     for (let i = 0; i < steps; i++) {
-        // matrix[headCurrentY][headCurrentX] = '.';
+        lastHeadX = headX;
+        lastHeadY = headY;
         switch (direction) {
             case 'U':
-                headCurrentY -= 1;
+                headY -= 1;
                 break;
             case 'L':
-                headCurrentX -= 1;
+                headX -= 1;
                 break;
             case 'D':
-                headCurrentY += 1;
+                headY += 1;
                 break;
             case 'R':
-                headCurrentX += 1;
+                headX += 1;
                 break;
         }
-        // matrix[headCurrentY][headCurrentX] = 'H';
 
-        // 2 consecutive moves in same direction
-        if (!lastDirection || lastDirection === direction) {
-            // matrix[tailCurrentY][tailCurrentX] = '.';
-            switch (direction) {
-                case 'U':
-                    // if on same axis AND detached then move
-                    if (isDetached(headCurrentX, headCurrentY, tailCurrentX, tailCurrentY) || !lastDirection) {
-                        tailCurrentY = headCurrentY + 1;
-                        tailCurrentX = headCurrentX;
-                    }
-                    break;
-                case 'L':
-                    if (isDetached(headCurrentX, headCurrentY, tailCurrentX, tailCurrentY) || !lastDirection) {
-                        tailCurrentX = headCurrentX + 1;
-                        tailCurrentY = headCurrentY;
-                    }
-                    break;
-                case 'D':
-                    if (isDetached(headCurrentX, headCurrentY, tailCurrentX, tailCurrentY) || !lastDirection) {
-                        tailCurrentY = headCurrentY - 1;
-                        tailCurrentX = headCurrentX;
-                    }
-                    break;
-                case 'R':
-                    if (isDetached(headCurrentX, headCurrentY, tailCurrentX, tailCurrentY)|| !lastDirection) {
-                        tailCurrentX = headCurrentX - 1;
-                        tailCurrentY = headCurrentY;
-                    }
-                    break;
-            }
-            // matrix[tailCurrentY][tailCurrentX] = 'T';
+        if (shouldFollowHead(lastDirection, direction, lastHeadX, lastHeadY, headX, headY, tailX, tailY)) {
+            tailX = lastHeadX;
+            tailY = lastHeadY;
         }
 
-        const coordinateString = [tailCurrentX, tailCurrentY].join(',');
+        lastDirection = direction;
+
+        const coordinateString = [tailX, tailY].join(',');
         if (!visitedByTail.length || visitedByTail.every(e => e !== coordinateString)) {
             visitedByTail.push(coordinateString);
         }
-        lastDirection = direction;
-        // await new Promise(resolve => setTimeout(_ => resolve(true), 500));
-        // printMatrix(matrix);
-        // console.log(tailCurrentX, tailCurrentY, visitedByTail);
+
+        if (showMatrix) {
+            const size = 100;
+            let matrix = [];
+            const row = Array(size).fill('.');
+            for (let i = 0; i < size; i++) {
+                matrix.push([...row]);
+            }
+            matrix[size - 1 - Math.abs(headY)][Math.abs(headX)] = 'H';
+            matrix[size - 1 - Math.abs(tailY)][Math.abs(tailX)] = 'T';
+            printMatrix(matrix);
+            await new Promise(resolve => setTimeout(_ => resolve(true), 250));
+        }
     }
 }
 
 console.log(`Part 1 result is: ${visitedByTail.length}`);
 debugger;
+
 // Part 2
 
 
 // Utils
+
+function shouldFollowHead(lastDirection: Direction, currentDirection: Direction, lastHeadX: number, lastHeadY: number, headCurrentX: number, headCurrentY: number, tailCurrentX: number, tailCurrentY: number): boolean {
+    if (!lastDirection) {
+        return true;
+    }
+    return isDetached(headCurrentX, headCurrentY, tailCurrentX, tailCurrentY);
+}
 
 function printMatrix(matrix: string[][]): void {
     console.clear();
@@ -112,8 +87,22 @@ function printMatrix(matrix: string[][]): void {
 }
 
 function isDetached(headCurrentX: number, headCurrentY: number, tailCurrentX: number, tailCurrentY: number) {
-    return ((headCurrentX - tailCurrentX) > 1 || (headCurrentX - tailCurrentX) < -1)
-        ||
-        ((headCurrentY - tailCurrentY) > 1 || (headCurrentY - tailCurrentY) < -1)
+    return Math.abs(headCurrentX - tailCurrentX) > 1 || Math.abs(headCurrentY - tailCurrentY) > 1;
 }
 
+async function getInstructions(inputFile: string): Promise<[Direction, number][]> {
+    const instructions: [Direction, number][] = [];
+    const readInterface = readline.createInterface({
+        input: fs.createReadStream(inputFile)
+    });
+
+    for await (const line of readInterface) {
+        input.push(line);
+    }
+    input.forEach(row => {
+        const [direction, steps] = row.split(' ');
+        instructions.push([direction as Direction, +steps]);
+    });
+
+    return instructions;
+}
